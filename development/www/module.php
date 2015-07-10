6a68a3190d48d12f1bb442bdc4660d457a426855
Dima Mudrakov <d.mudrakov@gmail.com>
2015-07-10 16:06:51 +0300

    Upload File 2
    With check

--- development/www/module.php
+++ development/www/module.php
@@ -1,6 +1,6 @@
 <?php
 
-    class file
+    class fileUpload
     {
         public function upLoadFile()
         {
@@ -8,24 +8,40 @@
             {
                 if(is_uploaded_file($_FILES['upload']['tmp_name']))
                 {
-                    copy($_FILES["upload"]["tmp_name"], "images/".$_FILES["upload"]["name"]);
-                    exit( "Фото загружено");
+                    if($_FILES['upload']['type'] == 'image/jpg' or $_FILES['upload']['type'] == 'image/jpeg' or $_FILES['upload']['type'] == 'image/png')
+                    {
+                        if($_FILES['upload']['size'] <= 1048576)
+                        {
+                            copy($_FILES["upload"]["tmp_name"], "images/".$_FILES["upload"]["name"]);
+                            exit( "Фото загружено");
+                        }
+                        else
+                        {
+                            echo "Размер файла не должен превышать 1мб";
+                        }
+                    }
+                    else
+                    {
+                        echo "Не верный формат файла";
+                    }
                 }
                 else
                 {
-                    echo 'Фото не загружено';
+                    echo "Выберите файл";
                 }
+
             }
             else
             {
                 echo "Выберите файл и нажмите загрузить фотографию";
             }
 
-
         }
     }
 
-    $file = new file;
+
+
+    $file = new fileUpload;
     $file -> upLoadFile();
 
 ?>
